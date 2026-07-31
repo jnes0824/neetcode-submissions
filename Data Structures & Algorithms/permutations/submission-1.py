cb2ed544987c759b316ecdf5be605cdb72d9e768
jnes0824 <89699286+jnes0824@users.noreply.github.com>
2026-07-31 15:19:04 +0800

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        ans = []
+        used = set()
+
+        def backtrack(num):
+            # 已經選了 n 個數字，得到一個 permutation
+            if len(num) == n:
+                ans.append(num.copy()) # O(N)
+                return
+
+            # 每一層都嘗試選擇所有尚未使用的數字
+            for i in range(n): # total n!
+                if i not in used:
+                    used.add(i)
+                    num.append(nums[i])
+
+                    backtrack(num)
+                    # backtracking：撤銷剛才的選擇
+                    num.pop()
+                    used.remove(i)
+
+        backtrack([])
+        return ans
